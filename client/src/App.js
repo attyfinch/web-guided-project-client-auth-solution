e8335fd5fc0891d82a8d9015466425cc736db19f
Warren C. Longmire <warren.longmire@lambdaschool.com>
2020-11-10 11:05:12 -0500

    adds logout code.

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,27 @@ import Login from "./components/Login";
 import GasPrices from "./components/GasPrices";
 import PrivateRoute from "./components/PrivateRoute";
 
+import axios from 'axios';
+
 function App() {
+  const logout = () => {
+    axios
+      .post("http://localhost:5000/api/logout", {userToken:localStorage.getItem('token')})
+      .then((res) => {
+        localStorage.removeItem("token");
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <Router>
       <div className="App">
         <ul>
           <li>
             <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link onClick={logout}>Logout</Link>
           </li>
           <li>
             <Link to="/protected">Protected Page</Link>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,6 @@ import React from "react";
 import axios from "axios";
 
 import { withRouter } from "react-router";
-import { withHistory } from "react-router-dom";
 
 class Login extends React.Component {
   state = {
@@ -22,13 +21,10 @@ class Login extends React.Component {
   };
 
   login = (e) => {
-    e.preventDefault();
-    // axiosWithAuth ==> ?? an axios instance; .post() ==> ?? promise
     axios
       .post("http://localhost:5000/api/login", this.state.credentials)
       .then((res) => {
         localStorage.setItem("token", res.data.payload);
-        // redirect to the apps main page?
         this.props.history.push("/protected");
       })
       .catch((err) => console.log(err));
